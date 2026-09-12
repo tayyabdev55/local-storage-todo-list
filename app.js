09617ac90283be32120d3e18bc4b692042ab1159
tayyabdev55 <tayyabkhan89807@gmail.com>
2026-09-12 17:52:14 +0630

    fg

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
-console.log("tayyab");
+let inp  = document.querySelector("#gtinp");
+let ul   = document.querySelector("#getul");
+
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function openForm() {
+    document.querySelector("#addtask").style.display = "block";
+}
+
+function closeForm() {
+    document.querySelector("#addtask").style.display = "none";
+}
+
+function addtodo(event) {
+
+    event.preventDefault();
+
+    todos.push(inp.value);
+
+    const str = JSON.stringify(todos);
+
+    localStorage.setItem("todos", str)
+
+    showtodos()
+
+}
+
+
+function showtodos() {
+    ul.innerHTML = "";
+
+    for (let i = 0; i < todos.length; i++) {
+        ul.innerHTML += `
+            <li>
+                <span>${todos[i]}</span>
+                <div class="task-actions">
+                    <button onclick="editTodo(${i})" title="Edit">
+                        <i class="fa-solid fa-pen"></i>
+                    </button>
+                    <button onclick="deltodo(${i})" title="Delete">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </li>
+        `;
+        const str = JSON.stringify(todos);
+        localStorage.setItem("todos", str)
+    }
+}
+
+
+showtodos()
+
+
+function deltodo(index) {
+    todos.splice(index, 1);
+
+
+    const str = JSON.stringify(todos);
+
+    localStorage.setItem("todos", str)
+
+
+    showtodos()
+}
+
+
+function editTodo(index) {
+    const newvalue = prompt("enetr new task", todos[index]);
+
+    todos[index] = newvalue
+
+    const str = JSON.stringify(todos);
+
+    localStorage.setItem("todos", str)
+
+
+    showtodos()
+}
